c3d431de495f1bd4058c845b7aba071c7bfab843
Jamaldeen Oluwasola Lawal <j.solalawal@gmail.com>
2022-07-01 01:30:49 -0700

    Hot Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - print the size of various types
-* Return: 0 if exited properly, non-zero otherwise
+* main - entry point
+* Return: 0 after printing the function
 */
 
 int main(void)
 {
-puts("\"programming is like building a multiliingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
