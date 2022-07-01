aa5698716b956a8a2a503cdb88c7bb3bea465b46
Jamaldeen Oluwasola Lawal <j.solalawal@gmail.com>
2022-07-01 01:15:07 -0700

    Task 2-6 & 100-101

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main(void)
+{
+puts("\"programming is like building a multiliingual puzzle");
+return (0);
+}
