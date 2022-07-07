59d1be7b0857bf3b8ace17356fc9ff6911f65221
Jamaldeen Oluwasola Lawal <j.solalawal@gmail.com>
2022-07-07 16:11:53 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - Multiplies two intergers
+* @x: The number to be checked
+* @y: The second number to mutiply with first number
+*
+* Return: The result of multiplication between x param and y param
+*/
+int mul(int x, int y)
+{
+return (x * y);
+}
