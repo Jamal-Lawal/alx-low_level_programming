1d2c974c36b6d4a45410babcd5a131eaf3efdc83
Jamaldeen Oluwasola Lawal <j.solalawal@gmail.com>
2022-07-07 17:30:53 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,18 +9,12 @@
 */
 void print_most_numbers(void)
 {
-int x = 0;
+int x;
 
-for (x <= 9; x++)
+for (x = 0; x < 10; x++)
 {
-if (x == 2 || x == 4)
-{
-continue;
+if (x != 2 && x != 4)
+_putchar(x + '0');
 }
-else
-{
-_putchar(x + '0')
-}
-}
-_putchar('\n);
+_putchar('\n');
 }
