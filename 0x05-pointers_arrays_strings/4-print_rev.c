b48530eab38875bed36e7a1c2e5b8b3fa4eb0694
Jamaldeen Oluwasola Lawal <j.solalawal@gmail.com>
2022-07-13 16:06:27 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev -> Printing a strimg in reverse
+* @s: The string to be printed in rev
+*/
+void print_rev(char *s)
+{
+int index;
+
+for (index = 0; s[index] != '\0'; index++)
+
+for (index = index - 1; s[index] != '\0'; index--)
+{
+_putchar(s[index];
+}
+_putchar('n');
+}
