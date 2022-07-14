82b69e7a41b5168fff1b2a0a292ef02a64519cd9
Jamaldeen Oluwasola Lawal <j.solalawal@gmail.com>
2022-07-13 19:30:38 -0700

    Task 4: Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,18 @@
 
 /**
 * print_rev -> Printing a strimg in reverse
+*
 * @s: The string to be printed in rev
 */
 void print_rev(char *s)
 {
-int index;
+	int index;
+	
+	for (index = 0; s[index] != '\0'; index++)
 
-for (index = 0; s[index] != '\0'; index++)
-
-for (index = index - 1; s[index] != '\0'; index--)
-{
-_putchar(s[index]);
-}
-_putchar('n');
+	for (index = index - 1; s[index] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
 }
